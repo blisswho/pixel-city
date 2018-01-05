6153735ba262496b5a90a164535e183568037f6e
Bliss <blisswho@yahoo.com>
2018-01-05 17:33:55 -0500

    Added Collection View

---
+++ pixel-city/View/PhotoCell.swift
@@ -0,0 +1,21 @@
+//
+//  PhotoCell.swift
+//  pixel-city
+//
+//  Created by Bliss Hu on 1/5/18.
+//  Copyright © 2018 Bliss Hu. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Init(coder:) has not been implemented")
+    }
+
+}
